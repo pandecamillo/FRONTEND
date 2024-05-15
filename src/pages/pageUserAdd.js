f8f0d95266742aa2339c5cea0f6a13b844cd1ebf
pandecamillo <160549468+pandecamillo@users.noreply.github.com>
2024-05-15 09:34:15 +0100

    Add files via upload

--- src/pages/pageUserAdd.js
+++ src/pages/pageUserAdd.js
@@ -1,59 +1,58 @@
-import React from 'react';
-import {useNavigate} from 'react-router-dom';
-import axios from 'axios';
-import defaultImage from '../components/defaultImage';
-import HeaderUser from '../components/headerUser';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import HeaderUser from "../components/headerUser";
+import connection from "../connection";
 
-function PageUserAdd(){
-    const [user, setUser] = React.useState({usernom:'', userprenom:'', userphone:'', userprofile:defaultImage});
-    
-    function handleChange(event){
-        setUser(prev=> ({...prev, [event.target.name] : event.target.value}))
+function PageUserAdd() {
+  const [user, setUser] = React.useState({
+    usernom: "",
+    userprenom: "",
+    userphone: "",
+  });
+
+  function handleChange(event) {
+    setUser((prev) => ({ ...prev, [event.target.name]: event.target.value }));
+  }
+
+  const navigate = useNavigate();
+
+  function goToApp() {
+    navigate("/");
+  }
+
+  async function addUser() {
+    if (user.usernom === "" || user.userprenom === "") {
+      alert("Vous devez remplir le champ");
+      return;
     }
-
-    const navigate = useNavigate() 
-
-    function goToApp(){
-      navigate("/");
+    if (!user.userphone) {
+      alert("Veuillez donner votre numero");
+      return;
     }
-
-
-
-    async function addUser(){
-        if(user.usernom === '' || user.userprenom === ''){
-          alert('Vous devez remplir le champ')
-          return;
-      }
-      if(!user.userphone){
-        alert("Veuillez donner votre numero");
-        return;
-      }
-      if(user.userphone.length != 9){
-        alert("Votre numero doit avoir 9 chiffres")
-        return;
-      }
-      if(!user.userprofile){
-        setUser(prev=> ({...prev, userprofile:defaultImage}))  
-      }
-      document.getElementById('btn-ajout').disabled = true;
-      try{
-        console.log('send request')
-        document.getElementById('info-message').style.visibility="visible";
-        await axios.post('https://backendutilisateurs.onrender.com/user', user)
-        document.getElementById('info-message').style.visibility="hidden"
-        console.log('get response')
-        goToApp();
-      }catch{
-        console.log("Erreur lors de l'ajout")
-        const appMessage = document.getElementById('app-message');
-        appMessage.innerHTML = requestFalureElement;
-          //document.getElementById("container")
-          //.innerHTML = "<p>Erreur lors de la connection au server.</p>";
-      }
+    if (user.userphone.length != 9) {
+      alert("Votre numero doit avoir 9 chiffres");
+      return;
     }
+    document.getElementById("btn-ajout").disabled = true;
+    try {
+      console.log("send request");
+      document.getElementById("info-message").style.visibility = "visible";
+      await axios.post(connection + "/user", user);
+      document.getElementById("info-message").style.visibility = "hidden";
+      console.log("get response");
+      goToApp();
+    } catch {
+      console.log("Erreur lors de l'ajout");
+      const appMessage = document.getElementById("app-message");
+      appMessage.innerHTML = requestFalureElement;
+      //document.getElementById("container")
+      //.innerHTML = "<p>Erreur lors de la connection au server.</p>";
+    }
+  }
 
-    const requestFalureElement = `<img
-                                        src="icons/no-wifi.png"
+  const requestFalureElement = `<img
+                                        src="../icons/no-wifi.png"
                                         style="width:100%;"
                                     />
                                     <p 
@@ -66,60 +65,94 @@ function PageUserAdd(){
                                     </p>
                                     <small style="font-weight:small;font-size:small; margin:20px; text-align:center">
                                         Veuillez réessayer plus tard
-                                    </small>`
+                                    </small>`;
 
-      function convertToBase64(file){
-        const reader = new FileReader();
-        reader.addEventListener(
-          "load",
-          () => {
-            setUser(user=>({...user, userprofile:reader.result}));
-            console.log(user)
-          },
-          false,
-        );
-      
-        if (file) {
-          reader.readAsDataURL(file);
-        }       
-      }
+  /* function convertToBase64(file) {
+    const reader = new FileReader();
+    reader.addEventListener(
+      "load",
+      () => {
+        setUser((user) => ({ ...user, userprofile: reader.result }));
+        console.log(user);
+      },
+      false
+    );
 
-      function importData() {
-        let input = document.createElement('input');
-        input.type = 'file';
-        input.onchange = _ => {
-          // you can use this method to get file and perform respective operations
-                  let files = Array.from(input.files)[0];
-                  convertToBase64(files);
-              };
-        input.click();      
-      }
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  }
 
-      function handlePhone(event){
-        if(user.userphone.length === 9){
-            console.log(user.userphone)
-            event.preventDefault();
-        } 
-      }
+  function importData() {
+    let input = document.createElement("input");
+    input.type = "file";
+    input.onchange = (_) => {
+      // you can use this method to get file and perform respective operations
+      let files = Array.from(input.files)[0];
+      convertToBase64(files);
+    };
+    input.click();
+  }
+  */
 
-    return(
-        <div className='app-container'>
-          <HeaderUser type="NOUVEAU UTILISATEUR"/>
-          <p className="info-message" id="info-message">Veuillez patienter ...</p>
-          <div className='form'>
-              <div className="user-profile-input">
-                <img id ="user-profile" className="user-profile" src={user.userprofile} alt="profile" onClick={importData}/>
-                <img src="icons/camera.png" alt="camera" className="btn-camera" onClick={importData}/>
-              </div>
-              <input autoComplete='off' maxLength={15}  type="text" name="usernom" onChange={handleChange} placeholder='Votre nom' value={user.usernom}/>
-              <input autoComplete='off' maxLength={15}  type="text" name="userprenom" onChange={handleChange} placeholder='Votre prenom' value={user.userprenom}/>
-              <div className="phone-input">
-                <label htmlFor="userphone">+243</label>
-                <input autoComplete='off' type="number" name="userphone" placeholder='Votre telephone' onChange={handleChange} onKeyPress={handlePhone} value={user.userphone}/>
-              </div>
-              <button className='btn-add-user' onClick={addUser} id="btn-ajout">AJOUTER</button>
-          </div>
-          </div>
-    )
+  function handlePhone(event) {
+    if (user.userphone.length === 9) {
+      console.log(user.userphone);
+      event.preventDefault();
+    }
+  }
+
+  return (
+    <div className="app-container">
+      <HeaderUser type="NOUVEAU AMI" />
+      <p className="info-message" id="info-message">
+        Veuillez patienter ...
+      </p>
+      <div className="form">
+        <div className="user-profile-input">
+          <img
+            id="user-profile"
+            className="user-profile"
+            src="img/user-profile.png"
+            alt="profile"
+            //onClick={importData}
+          />
+        </div>
+        <input
+          autoComplete="off"
+          maxLength={15}
+          type="text"
+          name="usernom"
+          onChange={handleChange}
+          placeholder="Votre nom"
+          value={user.usernom}
+        />
+        <input
+          autoComplete="off"
+          maxLength={15}
+          type="text"
+          name="userprenom"
+          onChange={handleChange}
+          placeholder="Votre prenom"
+          value={user.userprenom}
+        />
+        <div className="phone-input">
+          <label htmlFor="userphone">+243</label>
+          <input
+            autoComplete="off"
+            type="number"
+            name="userphone"
+            placeholder="Votre telephone"
+            onChange={handleChange}
+            onKeyPress={handlePhone}
+            value={user.userphone}
+          />
+        </div>
+        <button className="btn-add-user" onClick={addUser} id="btn-ajout">
+          AJOUTER
+        </button>
+      </div>
+    </div>
+  );
 }
 export default PageUserAdd;
