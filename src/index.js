aed799378eb20291d05157725cc804a8e1901e38
pandecamillo <160549468+pandecamillo@users.noreply.github.com>
2024-05-15 09:33:35 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PageApp from './pages/pageApp'
-import PageUserAdd from './pages/pageUserAdd'
-import PageUserEdit from './pages/pageUserEdit'
-import PageUserRead from './pages/pageUserRead';
-import Page404 from './pages/page404'
-import './style.css';
-import {HashRouter, Routes, Route} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PageApp from "./pages/pageApp";
+import PageUserAdd from "./pages/pageUserAdd";
+import PageUserEdit from "./pages/pageUserEdit";
+import PageUserRead from "./pages/pageUserRead";
+import Page404 from "./pages/page404";
+import "./style.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function PageRouter(){
-  return(
-    <HashRouter>
+function PageRouter() {
+  return (
+    <BrowserRouter>
       <Routes>
-          <Route path='/' element={<PageApp/>}/>
-          <Route path='new-user' element={<PageUserAdd/>}/>
-          <Route path='update-user/:id' element={<PageUserEdit/>}/>
-          <Route path='read-user/:id' element ={<PageUserRead/>}/>
-          <Route path='*' element={<Page404/>}/>
+        <Route path="/" element={<PageApp />} />
+        <Route path="new-user" element={<PageUserAdd />} />
+        <Route path="update-user/:id" element={<PageUserEdit />} />
+        <Route path="read-user/:id" element={<PageUserRead />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
-    </HashRouter>
-  )
+    </BrowserRouter>
+  );
 }
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-  );
-
-root.render(
-<PageRouter/>
-)
-
+root.render(<PageRouter />);
